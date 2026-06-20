82029329f953dc9b0e66a834af51ba272d66e1ef
久我伊知郎 <kugaichiro@koganoichirounoMacBook-Air.local>
2026-06-20 22:29:17 +0900

    add setting.js

---
+++ setting.js
@@ -0,0 +1,11 @@
+//複数のファイルで使われる大事な変数をここで定義
+const REMO_ACCESS_TOKEN = '';
+
+const SPREADSHEET_ID = ''
+
+//ここから下の変数はremoにアクセスするためのurlなので変更不可
+
+const REMO_OPERATION_URL = "https://api.nature.global/1/signals/";
+
+const REMO_DATA_URL = "https://api.nature.global/1/"
+
